8b74629394b2c7a9e517e91b26265cf98bfc0359
taorminak <ostanina.anna@yahoo.com>
2023-02-12 23:47:53 +0100

    React styles added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import styles from "./App.css";
-import Tariff from "./components/Tariff";
+import Tariff from "./components/Tariff/Tariff";
 import tariffs from "./components/tariffs.json";
+
 
 function App() {
   return (
     <div className={styles.App}>
-      {tariffs.map((tariff) => (
-        <Tariff
+      {tariffs.map((tariff, index) => (
+        <Tariff key={index}  
+        color={tariff.color}
           title={tariff.title}
           price={tariff.price}
           description={tariff.description}
--- src/components/Tariff/Tariff.jsx
+++ src/components/Tariff/Tariff.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "./tariff.css";
 
-export default function Tariff(props) {
+export default function Tariff(props, flag) {
+  const c1 = props.color=="red" ? "active" : "";
+
   return (
-    <div className={styles.Card}>
-      <div className={styles.Title}>{props.title}</div>
-      <div className={styles.Price}>
+    <div className={styles.Card + " " + styles[c1]}>
+      <div className={styles.Title + " " + styles[props.color]}>
+        {props.title}
+      </div>
+      <div className={styles.Price + " " + styles[props.color]}>
         <span className={styles.Rate}>руб.</span>
         {props.price}
         <span className={styles.Rate}>/мес.</span>

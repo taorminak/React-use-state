9df5b2b196a5b3e8878560034be7d9a99f7950e1
taorminak <ostanina.anna@yahoo.com>
2023-02-19 18:53:29 +0100

    State added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import styles from "./App.css";
 import Tariff from "./components/Tariff/Tariff";
 import tariffs from "./components/tariffs.json";
-
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className={styles.App}>
-      {tariffs.map((tariff, index) => (
-        <Tariff key={index}  
-        color={tariff.color}
-          title={tariff.title}
-          price={tariff.price}
-          description={tariff.description}
-          details={tariff.details}
+      {tariffs.map((item) => (
+        <Tariff
+          item={item}
+          key={item.id}
+          color={item.color}
+          title={item.title}
+          price={item.price}
+          description={item.description}
+          details={item.details}
+          setCount={setCount}
+          isBoolean={item.id == count ? true : false}
         />
       ))}
     </div>

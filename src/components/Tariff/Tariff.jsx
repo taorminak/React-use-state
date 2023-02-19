374db04a9bfd56cbdb23f0d88744a9b2402fccbb
taorminak <ostanina.anna@yahoo.com>
2023-02-19 18:55:19 +0100

    State changed

--- src/components/Tariff/Tariff.jsx
+++ src/components/Tariff/Tariff.jsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./tariff.css";
 
-export default function Tariff(props, flag) {
-  const c1 = props.color=="red" ? "active" : "";
+export default function Tariff(props) {
+  const { title, color, price, description, details, id } = props.item;
+
+  const c1 = props.isBoolean ? "active" : "";
+
+  function getId() {
+    console.log(id);
+    props.setCount(id);
+  }
 
   return (
-    <div className={styles.Card + " " + styles[c1]}>
-      <div className={styles.Title + " " + styles[props.color]}>
-        {props.title}
+    <div className={styles.Card + " " + styles[c1]} onClick={getId}>
+      <div className={styles.Title + " " + styles[color]} onClick={getId}>
+        {title}
       </div>
-      <div className={styles.Price + " " + styles[props.color]}>
+      <div className={styles.Price + " " + styles[color]}>
         <span className={styles.Rate}>руб.</span>
-        {props.price}
+        {price}
         <span className={styles.Rate}>/мес.</span>
       </div>
-      <div className={styles.Description}>{props.description}</div>
-      <div className={styles.Details}>{props.details}</div>
+      <div className={styles.Description}>{description}</div>
+      <div className={styles.Details}>{details}</div>
     </div>
   );
 }
